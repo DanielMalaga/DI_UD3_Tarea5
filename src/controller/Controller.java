43e3c8d4fde696c4faf9e614e4b32c19c573a82d
DanielMalaga <dan.aramita@gmail.com>
2023-01-15 23:06:03 +0100

    asociada vista y controlador e inicializado con acción boton

--- src/controller/Controller.java
+++ src/controller/Controller.java
@@ -1,5 +1,27 @@
 package controller;
 
-public class Controller {
+import java.net.URL;
+import java.util.ResourceBundle;
+
+import javafx.event.ActionEvent;
+import javafx.fxml.FXML;
+import javafx.fxml.Initializable;
+import javafx.scene.control.Button;
+
+public class Controller implements Initializable {
+
+	@FXML
+	private Button boton;
+
+	public void handleButtonAction(ActionEvent event) {
+		System.out.println("Botón pulsado");
+
+	}
+
+	@Override
+	public void initialize(URL arg0, ResourceBundle arg1) {
+		boton.setOnAction(this::handleButtonAction);
+
+	}
 
 }
