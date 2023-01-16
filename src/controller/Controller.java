583fdba47f798857d19969a5cafb43334c1d5141
DanielMalaga <dan.aramita@gmail.com>
2023-01-16 23:42:42 +0100

    añadidos más eventos al botón de ratón y de teclado

--- src/controller/Controller.java
+++ src/controller/Controller.java
@@ -7,6 +7,8 @@ import javafx.event.ActionEvent;
 import javafx.fxml.FXML;
 import javafx.fxml.Initializable;
 import javafx.scene.control.Button;
+import javafx.scene.input.KeyEvent;
+import javafx.scene.input.MouseEvent;
 
 public class Controller implements Initializable {
 
@@ -15,12 +17,29 @@ public class Controller implements Initializable {
 
 	public void handleButtonAction(ActionEvent event) {
 		System.out.println("Botón pulsado");
+	}
 
+	public void onMouseEntered(MouseEvent mouseEvent) {
+		System.out.println("entrado ratón en botón");
+	}
+
+	public void onMouseExited(MouseEvent mouseEvent) {
+		System.out.println("salido ratón de boton");
+	}
+
+	public void onKeyPressed(KeyEvent keyEvent) {
+		System.out.println("pulsado botón con tecla");
 	}
 
 	@Override
 	public void initialize(URL arg0, ResourceBundle arg1) {
 		boton.setOnAction(this::handleButtonAction);
+
+		boton.setOnMouseEntered(this::onMouseEntered);
+
+		boton.setOnMouseExited(this::onMouseExited);
+
+		boton.setOnKeyPressed(this::onKeyPressed);
 
 	}
 
